8edc15086757b9c5fbd6390e24cefabd924bd403
Mike Bostock <mbostock@gmail.com>
2017-10-09 16:16:56 -0700

    Use JSDOM.

--- test/builtins-test.js
+++ test/builtins-test.js
@@ -1,14 +1,15 @@
+import {JSDOM} from "jsdom";
 import tape from "tape-await";
 import {runtime as createRuntime} from "../";
 import "./requestAnimationFrame";
 
 tape("variable.define does not allow a variable to mask a builtin", async test => {
   let result;
+  const document = new JSDOM(`<div id=v1></div><div id=v2></div>`).window.document;
   const runtime = createRuntime({color: "red"});
   const module = runtime.module();
-  const v1 = module.variable().define("color", () => test.fail());
-  const v2 = module.variable().define(["color"], color => result = color);
-  v1._outdegree = v2._outdegree = 1; // TODO Cleaner way to force computation?
+  module.variable(document.querySelector("#v1")).define("color", () => test.fail());
+  module.variable(document.querySelector("#v2")).define(["color"], color => result = color);
   await new Promise(setImmediate);
   test.equal(result, "red");
 });
