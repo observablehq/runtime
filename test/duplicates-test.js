c5be0ee95db388e14bbba9552dfdb382ad8af701
Mike Bostock <mbostock@gmail.com>
2017-11-16 16:22:36 -0800

    Test for duplicates.

--- test/duplicates-test.js
+++ test/duplicates-test.js
@@ -1,16 +1,16 @@
-import {JSDOM} from "jsdom";
-import tape from "tape-await";
 import {runtime as createRuntime} from "../";
 import "./requestAnimationFrame";
+import tape from "./tape";
+import valueof from "./valueof";
 
-tape("duplicate declarations", async test => {
-  let result;
-  const document = new JSDOM(`<div id=v1></div><div id=v2></div><div id=v3></div>`).window.document;
+tape("variable.define detects duplicate declarations", {html: "<div id=v1></div><div id=v2></div><div id=v3></div>"}, async test => {
   const runtime = createRuntime();
   const module = runtime.module();
-  module.variable(document.querySelector("#v1")).define("foo", 1);
-  module.variable(document.querySelector("#v2")).define("foo", 2);
-  module.variable(document.querySelector("#v3")).define(["foo"], err => result = err);
+  const v1 = module.variable("#v1").define("foo", 1);
+  const v2 = module.variable("#v2").define("foo", 2);
+  const v3 = module.variable("#v3").define(["foo"], foo => foo);
   await new Promise(setImmediate);
-  test.equal(result, "red");
+  test.deepEqual(await valueof(v1), {error: "foo is defined more than once"});
+  test.deepEqual(await valueof(v2), {error: "foo is defined more than once"});
+  test.deepEqual(await valueof(v3), {error: "foo is defined more than once"});
 });
