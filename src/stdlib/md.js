08dea50f74f129ec563f25de7b17126de319f9cf
Mike Bostock <mbostock@gmail.com>
2017-11-25 15:54:33 -0800

    Trim generated HTML before parsing.
    Marked generates a trailing newline, causing a spurious text node at the end,
    breaking the root.childNodes.length === 1 test.

--- src/stdlib/md.js
+++ src/stdlib/md.js
@@ -5,7 +5,7 @@ export default function(require) {
         var string = strings[0] + "", i = 0, n = arguments.length;
         while (++i < n) string += arguments[i] + "" + strings[i];
         var root = document.createElement("div");
-        root.innerHTML = marked.parse(string);
+        root.innerHTML = marked(string).trim();
         var code = root.querySelectorAll("pre code");
         if (code.length > 0) require("https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js").then(function(hl) { code.forEach(hl.highlightBlock); });
         return root.childNodes.length === 1 ? root.removeChild(root.firstChild) : root;
