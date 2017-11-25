ea9456a77d5f06a055102fcbdb005b82578fed02
Mike Bostock <mbostock@gmail.com>
2017-11-25 09:58:54 -0800

    Simplify definitions.
    These variables are defined as functions which are lazily evaluated, so there’s
    no longer a need to implement them as lazy promises. Also, require caches
    internally, so there’s also no need to cache required libraries internally.

--- src/stdlib/md.js
+++ src/stdlib/md.js
@@ -1,21 +1,15 @@
 export default function(require) {
-  var md, hl;
-
-  function marked(marked) {
-    return function(strings) {
-      var string = strings[0] + "", i = 0, n = arguments.length;
-      while (++i < n) string += arguments[i] + "" + strings[i];
-      var root = document.createElement("div");
-      root.innerHTML = marked.parse(string);
-      var code = root.querySelectorAll("pre code");
-      if (code.length > 0) (hl || (hl = require("https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js"))).then(function(hl) { code.forEach(hl.highlightBlock); });
-      return root.childNodes.length === 1 ? root.removeChild(root.firstChild) : root;
-    };
-  }
-
-  return {
-    then: function(resolved, rejected) {
-      return (md || (md = require("marked@0.3.6/marked.min.js").then(marked))).then(resolved, rejected);
-    }
+  return function() {
+    return require("marked@0.3.6/marked.min.js").then(function(marked) {
+      return function(strings) {
+        var string = strings[0] + "", i = 0, n = arguments.length;
+        while (++i < n) string += arguments[i] + "" + strings[i];
+        var root = document.createElement("div");
+        root.innerHTML = marked.parse(string);
+        var code = root.querySelectorAll("pre code");
+        if (code.length > 0) require("https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js").then(function(hl) { code.forEach(hl.highlightBlock); });
+        return root.childNodes.length === 1 ? root.removeChild(root.firstChild) : root;
+      };
+    });
   };
 }
--- src/stdlib/tex.js
+++ src/stdlib/tex.js
@@ -1,28 +1,28 @@
+function style(href) {
+  return new Promise(function(resolve, reject) {
+    var link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = href;
+    link.onerror = reject;
+    link.onload = resolve;
+    document.head.appendChild(link);
+  });
+}
+
 export default function(require, resource) {
-  var tex;
-
-  function katex(katex) {
-    return new Promise(function(resolve, reject) {
-      var link = document.createElement("link");
-      link.rel = "stylesheet";
-      link.href = resource("katex@0.8.3/dist/katex.min.css");
-      link.onerror = reject;
-      link.onload = function() {
-        resolve(function(strings) {
-          var string = strings[0] + "", i = 0, n = arguments.length;
-          while (++i < n) string += arguments[i] + "" + strings[i];
-          var root = document.createElement("div");
-          katex.render(string, root);
-          return root;
-        });
+  return function() {
+    return Promise.all([
+      require("katex@0.8.3/dist/katex.min.js"),
+      style(resource("katex@0.8.3/dist/katex.min.css"))
+    ]).then(function(values) {
+      var katex = values[0];
+      return function(strings) {
+        var string = strings[0] + "", i = 0, n = arguments.length;
+        while (++i < n) string += arguments[i] + "" + strings[i];
+        var root = document.createElement("div");
+        katex.render(string, root);
+        return root;
       };
-      document.head.appendChild(link);
     });
-  }
-
-  return {
-    then: function(resolved, rejected) {
-      return (tex || (tex = require("katex@0.8.3/dist/katex.min.js").then(katex))).then(resolved, rejected);
-    }
   };
 }
