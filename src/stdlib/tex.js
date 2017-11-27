ab7a156c11dcfaa58ee35c049c6f828168d52db8
Mike Bostock <mbostock@gmail.com>
2017-11-27 11:18:49 -0800

    Self-host required libraries.

--- src/stdlib/tex.js
+++ src/stdlib/tex.js
@@ -12,8 +12,8 @@ function style(href) {
 export default function(require, resource) {
   return function() {
     return Promise.all([
-      require("katex@0.8.3/dist/katex.min.js"),
-      style(resource("katex@0.8.3/dist/katex.min.css"))
+      require("/katex.js"),
+      style(resource("/katex.css"))
     ]).then(function(values) {
       var katex = values[0];
       return function(strings) {
