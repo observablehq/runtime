44b4e360a1699c38e9ec174a3faacfff918c13fc
Mike Bostock <mbostock@gmail.com>
2017-12-04 12:16:07 -0800

    Var, not const.

--- src/stdlib/dom/download.js
+++ src/stdlib/dom/download.js
@@ -1,10 +1,10 @@
 export default function(object, name, label) {
-  const a = document.createElement("a");
-  const button = a.appendChild(document.createElement("button"));
-  button.textContent = label == null ? "Download" : label;
+  var a = document.createElement("a"),
+      b = a.appendChild(document.createElement("button"));
+  b.textContent = label == null ? "Download" : label;
   a.download = name == null ? "untitled" : name;
   a.onclick = function() {
-    const url = a.href = URL.createObjectURL(object);
+    var url = a.href = URL.createObjectURL(object);
     setTimeout(function() { URL.revokeObjectURL(url); }, 50);
   };
   return a;
