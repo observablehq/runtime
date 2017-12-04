2276dca74bbd796e32e04321125e80bb7f9d4030
Mike Bostock <mbostock@gmail.com>
2017-12-04 09:54:11 -0800

    Set viewBox. Fixes #31.

--- src/stdlib/dom/svg.js
+++ src/stdlib/dom/svg.js
@@ -1,5 +1,6 @@
 export default function(width, height) {
   var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  svg.setAttribute("viewBox", [0, 0, width, height]);
   svg.setAttribute("width", width);
   svg.setAttribute("height", height);
   return svg;
