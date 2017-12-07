8001a46fad5d664d23ee3d8a5f9ed1b59a06dc2d
jashkenas <jashkenas@gmail.com>
2017-12-07 15:30:20 -0800

    Refactor out ResolutionError to separate file, ES5.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export {default as runtime} from "./runtime";
 export {default as runtimeLibrary} from "./stdlib/index";
 export {default as runtimeImport} from "./inspect/import";
+export {ResolutionError} from "./errors";
