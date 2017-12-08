60f4fd1012072794a18a8ded527aa63c669069d8
jashkenas <jashkenas@gmail.com>
2017-12-07 16:29:31 -0800

    Ensure that event.detail is always an object from dispatch.

--- src/dispatch.js
+++ src/dispatch.js
@@ -1,11 +1,12 @@
 export default function dispatch(node, type, detail) {
+  detail = detail || {};
   var document = node.ownerDocument, event = document.defaultView.CustomEvent;
   if (typeof event === "function") {
     event = new event(type, {detail});
   } else {
     event = document.createEvent("Event");
     event.initEvent(type, false, false);
-    event.detail = detail || {};
+    event.detail = detail;
   }
   node.dispatchEvent(event);
 }
