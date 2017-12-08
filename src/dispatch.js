588290811b33f0bb32031fce6e7fd77aa80177d9
jashkenas <jashkenas@gmail.com>
2017-12-08 11:10:11 -0800

    Remove object literal shorthand.

--- src/dispatch.js
+++ src/dispatch.js
@@ -2,7 +2,7 @@ export default function dispatch(node, type, detail) {
   detail = detail || {};
   var document = node.ownerDocument, event = document.defaultView.CustomEvent;
   if (typeof event === "function") {
-    event = new event(type, {detail});
+    event = new event(type, {detail: detail});
   } else {
     event = document.createEvent("Event");
     event.initEvent(type, false, false);
