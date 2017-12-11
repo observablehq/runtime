4b124498ea2d4b5f21e811e8f434fe3b6cb64ebf
Mike Bostock <mbostock@gmail.com>
2017-12-11 15:45:50 -0800

    Shorter with reduce.

--- src/runtime.js
+++ src/runtime.js
@@ -110,13 +110,12 @@ function variable_value(variable) {
   return variable._value.catch(variable._rejector);
 }
 
+function variable_version(version, variable) {
+  return Math.max(variable._version, version);
+}
+
 function variable_compute(variable) {
-  var version = variable._version;
-  variable._inputs.forEach(function(input) {
-    if (input._version > version) {
-      variable._version  = version = input._version;
-    }
-  });
+  var version = variable._version = variable._inputs.reduce(variable_version, variable._version);
   if (variable._generator) {
     variable._generator.return();
     variable._generator = null;
