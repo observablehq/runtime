0b665ca3f90f3d3a9f9f4c51de5dda247a0c735b
Mike Bostock <mbostock@gmail.com>
2017-12-12 16:16:44 -0800

    Add MathML namespace.

--- src/stdlib/dom/element.js
+++ src/stdlib/dom/element.js
@@ -1,4 +1,5 @@
 var namespaces = {
+  math: "http://www.w3.org/1998/Math/MathML",
   svg: "http://www.w3.org/2000/svg",
   xhtml: "http://www.w3.org/1999/xhtml",
   xlink: "http://www.w3.org/1999/xlink",
