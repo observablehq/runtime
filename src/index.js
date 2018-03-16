09d97a7a9bc1ebd15669cc0bec12606e2cf7a6a8
jashkenas <jashkenas@gmail.com>
2018-03-16 10:07:57 -0700

    Initial notebook load, switch to esm

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 export {default as runtime} from "./runtime";
 export {RuntimeError} from "./errors";
+
+export function load(stdlib, notebook) {
+  const runtime = runtime(stdlib);
+  const module = runtime.module();
+  notebook.cells.forEach(cell => {
+    const variable = module.variable();
+    variable.define(cell.name, cell.inputs, cell.definition);
+  });
+  return runtime;
+}
