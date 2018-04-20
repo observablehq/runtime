7252092d302ff2a4ea325293d2316715593fb262
jashkenas <jashkenas@gmail.com>
2018-04-20 14:13:15 -0700

    Moving the  function to be a class function on Notebook, instead of top-level.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,4 @@
 export {RuntimeError} from "./errors";
 export {default as Library} from "./library";
-import {default as _Notebook} from "./notebook";
+export {default as Notebook} from "./notebook";
 export {default as Runtime} from "./runtime";
-export const Notebook = _Notebook;
-
-export function load(definition, elements) {
-  const notebook = new _Notebook(`${definition.slug}@${definition.version}`);
-  definition.cells.forEach(cell => {
-    if (cell.name in elements) cell.node = elements[cell.name];
-    notebook.cell(cell.node).define(cell);
-  });
-  return notebook;
-}
