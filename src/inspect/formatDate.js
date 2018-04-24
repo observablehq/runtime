1f6ef2a4329b8eeb9fa1d9f4c53382e0c7c7344b
Mike Bostock <mbostock@gmail.com>
2018-04-24 11:41:06 -0700

    Remove d3-time-format dependency.

--- src/inspect/formatDate.js
+++ src/inspect/formatDate.js
@@ -1,14 +1,24 @@
-import {timeFormat} from "d3-time-format";
-
-var formatDay = timeFormat("%Y-%m-%d"),
-    formatMinute = timeFormat("%Y-%m-%dT%H:%M"),
-    formatSecond = timeFormat("%Y-%m-%dT%H:%M:%S"),
-    formatMillisecond = timeFormat("%Y-%m-%dT%H:%M:%S.%L");
+function pad(value, width) {
+  var s = value + "", length = s.length;
+  return length < width ? new Array(width - length + 1).join(0) + s : s;
+}
 
 export default function formatDate(date) {
-  return isNaN(date) ? "Invalid Date"
-      : (date.getMilliseconds() ? formatMillisecond
-          : date.getSeconds() ? formatSecond
-          : date.getMinutes() || date.getHours() ? formatMinute
-          : formatDay)(date);
+  if (isNaN(date)) return "Invalid Date";
+  return pad(date.getFullYear(), 4) + "-"
+      + pad(date.getMonth() + 1, 2) + "-"
+      + pad(date.getDate(), 2)
+      + (date.getMilliseconds() ? "T"
+          + pad(date.getHours(), 2) + ":"
+          + pad(date.getMinutes(), 2) + ":"
+          + pad(date.getSeconds(), 2) + "."
+          + pad(date.getMilliseconds(), 4)
+        : date.getSeconds() ? "T"
+          + pad(date.getHours(), 2) + ":"
+          + pad(date.getMinutes(), 2) + ":"
+          + pad(date.getSeconds(), 2)
+        : date.getMinutes() || date.getHours() ? "T"
+          + pad(date.getHours(), 2) + ":"
+          + pad(date.getMinutes(), 2)
+        : "");
 }
