98cc4844d1cda79e08070d654637e8b92f3bef97
jashkenas <jashkenas@gmail.com>
2018-04-26 13:10:42 -0700

    Use view and mutable flags.

--- src/index.js
+++ src/index.js
@@ -9,31 +9,35 @@ export function load(notebookModule, nodes = {}) {
   const library = new Library();
   const runtime = new Runtime(library);
   const moduleMap = new Map();
-  const {Generators} = library;
+  const {Generators: {input}} = library;
 
   modules.forEach(m =>  moduleMap.set(m.id, runtime.module()));
 
   modules.forEach(m => {
     const module = moduleMap.get(m.id);
+
+    function module_variable(name) {
+      const node = m.id === notebookModule.main ? nodes[name] : null;
+      return module.variable(node);
+    }
+
     m.variables.forEach(v => {
-      const node = m.id === notebookModule.main ? nodes[v.name] : null;
-      const variable = module.variable(node);
-      if (v.flag) {
-        v.value = flag_value(v.flag, v.value);
-      }
-      if (v.from) {
-        variable.import(v.name, v.remote, moduleMap.get(v.from));
+      if (v.view) {
+        const reference = `viewof ${v.name}`;
+        module_variable(reference).define(reference, v.inputs, v.value);
+        module_variable(v.name).define(v.name, [reference], input);
+      } else if (v.mutable) {
+        const reference = `mutable ${v.name}`;
+        module_variable(reference).define(reference, v.inputs, Mutable.value(v.value));
+        module_variable(v.name).define(v.name, [reference], mutable => mutable.generator);
       } else {
-        variable.define(v.name, v.inputs, v.value);
+        const variable = module_variable(v.name);
+        if (v.from) {
+          variable.import(v.name, v.remote, moduleMap.get(v.from));
+        } else {
+          variable.define(v.name, v.inputs, v.value);
+        }
       }
     });
   });
-
-  function flag_value(flag, value) {
-    switch (flag) {
-      case "view-value": return Generators.input;
-      case "mutable": return Mutable.value(value);
-      case "mutable-value": return _ => _.generator;
-    }
-  }
 }
