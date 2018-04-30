fb2de62e046bff3336c77a18ece8f1756b6d2cf4
jashkenas <jashkenas@gmail.com>
2018-04-30 10:22:58 -0700

    Working version of stdlib view and mutable / simpler runtime.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-import {Library, Mutable} from "@observablehq/notebook-stdlib";
+import {Library} from "@observablehq/notebook-stdlib";
 import {default as Runtime} from "./runtime";
 
 export {RuntimeError} from "./errors";
-export {Library, Runtime, Mutable};
+export {Library, Runtime};
 
 export function load(notebook, nodes = {}) {
   const {modules} = notebook;
   const library = new Library();
   const runtime = new Runtime(library);
   const moduleMap = new Map();
-  const {Generators: {input}} = library;
 
   modules.forEach(m =>  moduleMap.set(m.id, runtime.module()));
 
@@ -23,21 +22,11 @@ export function load(notebook, nodes = {}) {
     }
 
     m.variables.forEach(v => {
-      if (v.view) {
-        const reference = `viewof ${v.name}`;
-        module_variable(reference).define(reference, v.inputs, v.value);
-        module_variable(v.name).define(v.name, [reference], input);
-      } else if (v.mutable) {
-        const reference = `mutable ${v.name}`;
-        module_variable(reference).define(reference, v.inputs, Mutable.value(v.value));
-        module_variable(v.name).define(v.name, [reference], mutable => mutable.generator);
+      const variable = module_variable(v.name);
+      if (v.from) {
+        variable.import(v.name, v.remote, moduleMap.get(v.from));
       } else {
-        const variable = module_variable(v.name);
-        if (v.from) {
-          variable.import(v.name, v.remote, moduleMap.get(v.from));
-        } else {
-          variable.define(v.name, v.inputs, v.value);
-        }
+        variable.define(v.name, v.inputs, v.value);
       }
     });
   });
