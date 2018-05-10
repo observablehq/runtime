44f0d1e1ab4e4b74533bac110402320e5f1238e1
jashkenas <jashkenas@gmail.com>
2018-05-10 12:57:30 -0700

    Updating bundling strategy to minified ES module + minified UMD.

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,30 @@
 import node from "rollup-plugin-node-resolve";
+import uglify from "rollup-plugin-uglify";
 
-export default {
-  input: "src/index.js",
-  plugins: [
-    node()
-  ],
-  output: {
-    extend: true,
-    banner: `// @observablehq/notebook-runtime Copyright ${(new Date).getFullYear()} Observable, Inc.`,
-    file: "dist/notebook-runtime.js",
-    format: "umd",
-    name: "Observable"
-  }
+const defaultOutput = {
+  banner: `// @observablehq/notebook-runtime Copyright ${(new Date).getFullYear()} Observable, Inc.`,
 };
+
+function config(output) {
+  return {
+    input: "src/index.js",
+    plugins: [
+      node(),
+      uglify()
+    ],
+    output: Object.assign(output, defaultOutput)
+  };
+}
+
+export default [
+  config({
+    format: "es",
+    file: "dist/notebook-runtime.js"
+  }),
+  config({
+    format: "umd",
+    extend: true,
+    name: "Observable",
+    file: "dist/notebook-runtime.umd.js"
+  })
+];
