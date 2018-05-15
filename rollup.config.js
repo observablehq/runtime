296f126a54ef0a0dd6dcecb5c5ed8ea7f73d841c
jashkenas <jashkenas@gmail.com>
2018-05-15 09:50:49 -0700

    Surprisingly tricky: Fix rollup bundling
    Our notebook-inspector and notebook-stdlib export the
    ES module as package.json's "main", whereas d3-require
    has the built UMD as it's "main". Inspector and stdlib have
    an unbuilt dist/ ES module as their "module", whereas
    d3-require has the raw unbundled ES module entry point as its
    "module".
    
    Solved by using "main" in the rollup config, and adding extra
    configuration so that rollup can parse the UMD flavor of d3-require.

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import node from "rollup-plugin-node-resolve";
 import uglify from "rollup-plugin-uglify";
+import commonjs from "rollup-plugin-commonjs";
 
 const defaultOutput = {
   banner: `// @observablehq/notebook-runtime Copyright ${(new Date).getFullYear()} Observable, Inc.`,
@@ -9,7 +10,12 @@ function config(output) {
   return {
     input: "src/index.js",
     plugins: [
-      node(),
+      node({module: false, main: true}),
+      commonjs({
+        namedExports: {
+          "d3-require/dist/d3-require.js": ["require", "requireFrom"]
+        }
+      }),
       uglify()
     ],
     output: Object.assign(output, defaultOutput)
