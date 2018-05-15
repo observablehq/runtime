d6debaaf544b824ba31bf0054769f3241d1943fb
jashkenas <jashkenas@gmail.com>
2018-05-15 11:50:54 -0700

    Switching to main:umd and module:esm bundling.

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,5 @@
 import node from "rollup-plugin-node-resolve";
 import uglify from "rollup-plugin-uglify";
-import commonjs from "rollup-plugin-commonjs";
 
 const defaultOutput = {
   banner: `// @observablehq/notebook-runtime Copyright ${(new Date).getFullYear()} Observable, Inc.`,
@@ -10,12 +9,7 @@ function config(output) {
   return {
     input: "src/index.js",
     plugins: [
-      node({module: false, main: true}),
-      commonjs({
-        namedExports: {
-          "d3-require/dist/d3-require.js": ["require", "requireFrom"]
-        }
-      }),
+      node(),
       uglify()
     ],
     output: Object.assign(output, defaultOutput)
