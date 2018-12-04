7a9853a7f3ccae2c6e1f71a67ed17ec9d829b35f
Tom MacWright <tom@macwright.org>
2018-12-03 16:39:56 -0800

    Name errors as well

--- src/variable.js
+++ src/variable.js
@@ -187,5 +187,5 @@ function variable_fulfilled(value) {
 }
 
 function variable_rejected(error) {
-  if (this._observer.rejected) this._observer.rejected(error);
+  if (this._observer.rejected) this._observer.rejected(error, this._name);
 }
