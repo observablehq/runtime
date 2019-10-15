5b67cf3667688e26de0c4ffda86103216496c1e1
Mike Bostock <mbostock@gmail.com>
2019-10-14 19:35:47 -0700

    Update special-test.js

--- test/module/special-test.js
+++ test/module/special-test.js
@@ -1,9 +1,9 @@
-import { Runtime } from "../../src";
+import {Runtime} from "../../src";
 import tape from "../tape";
 import valueof from "../variable/valueof";
 import noop from "../../src/noop";
 
-tape("module._special defines a module local variable", async test => {
+tape("runtime.module(..., special) defines a module special variable", async test => {
   const runtime = new Runtime();
   const module = runtime.module(noop, undefined, {foo: () => 42});
   const bar = module.variable(true).define("bar", ["foo"], foo => foo + 1);
@@ -11,12 +11,19 @@ tape("module._special defines a module local variable", async test => {
   test.deepEqual(await valueof(bar), {value: 43});
 });
 
-tape("module._special does not override a normal variable", async test => {
+tape("runtime.module(..., special) can be a constant", async test => {
   const runtime = new Runtime();
-  const module = runtime.module();
+  const module = runtime.module(noop, undefined, {foo: 42});
+  const bar = module.variable(true).define("bar", ["foo"], foo => foo + 1);
+  await new Promise(setImmediate);
+  test.deepEqual(await valueof(bar), {value: 43});
+});
+
+tape("runtime.module(..., special) can be overridden by a normal variable", async test => {
+  const runtime = new Runtime();
+  const module = runtime.module(noop, undefined, {foo: 0});
   const foo = module.variable(true).define("foo", [], () => 42);
   const bar = module.variable(true).define("bar", ["foo"], foo => foo + 1);
-  module._special.set("foo", () => 0);
   await new Promise(setImmediate);
   test.deepEqual(await valueof(foo), {value: 42});
   test.deepEqual(await valueof(bar), {value: 43});
