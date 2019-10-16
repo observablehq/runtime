4fb3d19398908d81dde8b3b9fbe51b7e8651c5b3
Mike Bostock <mbostock@gmail.com>
2019-10-15 17:48:29 -0700

    More deterministic tests.

--- test/variable/valueof.js
+++ test/variable/valueof.js
@@ -1,5 +1,5 @@
 export default async function valueof(variable) {
-  await variable._module._runtime._computing;
+  await variable._module._runtime._compute();
   try { return {value: await variable._promise}; }
   catch (error) { return {error: error.toString()}; }
 }
