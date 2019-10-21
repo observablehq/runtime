87df00cb211dd18dd650f71553dc36528d246c82
jashkenas <jashkenas@gmail.com>
2019-10-21 12:18:00 -0700

    Exporting FileAttachment in parallel to Library

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import {Inspector} from "@observablehq/inspector";
-import {Library} from "@observablehq/stdlib";
+import {Library, FileAttachment} from "@observablehq/stdlib";
 import {RuntimeError} from "./errors";
 import Runtime from "./runtime";
 
-export {Inspector, Library, Runtime, RuntimeError};
+export {Inspector, Library, FileAttachment, Runtime, RuntimeError};
