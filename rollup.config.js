6e829fbf81016d84ae621de27220394e6e12df00
Mike Bostock <mbostock@gmail.com>
2021-05-26 11:41:58 -0700

    don’t mangle SQLiteDatabaseClient (#294)

--- rollup.config.js
+++ rollup.config.js
@@ -12,7 +12,7 @@ function config(output) {
       terser({
         toplevel: output.format === "es",
         output: {preamble: copyright},
-        mangle: {reserved: ["FileAttachment", "RequireError"]}
+        mangle: {reserved: ["FileAttachment", "RequireError", "SQLiteDatabaseClient"]}
       })
     ],
     output
