53099f150f46c510979d5203c5959360b8efccf2
Mike Bostock <mbostock@gmail.com>
2021-10-03 09:58:33 -0700

    reduce promise depth if no inputs

--- src/runtime.js
+++ src/runtime.js
@@ -206,10 +206,21 @@ function variable_compute(variable) {
   variable._invalidate();
   variable._invalidate = noop;
   variable._pending();
-  var value0 = variable._value,
-      version = ++variable._version,
-      invalidation = null,
-      promise = variable._promise = Promise.all(variable._inputs.map(variable_value)).then(function(inputs) {
+
+  const value0 = variable._value;
+  const version = ++variable._version;
+
+  // Lazily-constructed invalidation variable; only constructed if referenced as an input.
+  let invalidation = null;
+
+  // If the variable doesn’t have any inputs, we can optimize slightly.
+  const promise = variable._promise = (variable._inputs.length
+      ? Promise.all(variable._inputs.map(variable_value)).then(define)
+      : new Promise(resolve => resolve(variable._definition.call(value0))))
+    .then(generate);
+
+  // Compute the initial value of the variable.
+  function define(inputs) {
     if (variable._version !== version) return;
 
     // Replace any reference to invalidation with the promise, lazily.
@@ -227,20 +238,22 @@ function variable_compute(variable) {
       }
     }
 
-    // Compute the initial value of the variable.
     return variable._definition.apply(value0, inputs);
-  }).then(function(value) {
-    // If the value is a generator, then retrieve its first value,
-    // and dispose of the generator if the variable is invalidated.
-    // Note that the cell may already have been invalidated here,
-    // in which case we need to terminate the generator immediately!
+  }
+
+  // If the value is a generator, then retrieve its first value, and dispose of
+  // the generator if the variable is invalidated. Note that the cell may
+  // already have been invalidated here, in which case we need to terminate the
+  // generator immediately!
+  function generate(value) {
     if (generatorish(value)) {
       if (variable._version !== version) return void value.return();
       (invalidation || variable_invalidator(variable)).then(variable_return(value));
       return variable_generate(variable, version, value);
     }
     return value;
-  });
+  }
+
   promise.then(function(value) {
     if (variable._version !== version) return;
     variable._value = value;
